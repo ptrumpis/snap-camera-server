43fd6c6421a68f257ab346b66a63f68aafdbeccf
Patrick Trumpis <patrick.trumpis@gmx.de>
2023-04-04 20:45:17 +0200

    New YAML based configuration file

---
+++ src/utils/config.js
@@ -0,0 +1,16 @@
+import YAML from 'yaml';
+import * as fs from "fs/promises";
+
+const Config = await loadConfig();
+
+async function loadConfig() {
+    try {
+        const file = await fs.readFile("config.yml", { encoding: 'utf8' });
+        return YAML.parse(file);
+    } catch (e) {
+        console.error(e);
+    }
+    return {};
+}
+
+export { Config }
