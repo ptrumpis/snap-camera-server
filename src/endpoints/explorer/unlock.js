45bfa646a4fde8fecc2a5532254c47eefbabad53
Patrick Trumpis <patrick.trumpis@gmx.de>
2025-01-30 15:12:11 +0100

    Rework unlock.js

--- src/endpoints/explorer/unlock.js
+++ src/endpoints/explorer/unlock.js
@@ -15,42 +15,55 @@ router.get('/', async function (req, res, next) {
         return res.json({});
     }
 
-    let unlock = await DB.getLensUnlock(lensId);
+    const unlock = await DB.getLensUnlock(lensId);
     if (unlock && unlock[0]) {
         // trigger re-download to catch missing files automatically
         await Util.downloadUnlock(unlock[0].lens_id, unlock[0].lens_url);
+
         return res.json(Util.modifyResponseURLs(unlock[0]));
-    }
-
-    if (useRelay) {
-        unlock = await Util.getUnlockFromRelay(lensId);
-        if (unlock) {
-            DB.insertUnlock(unlock);
-            return res.json(unlock);
-        }
-    }
-
-    if (useWebSource) {
-        // try to retrive unlock information by UUID
-        let lens = Web.Cache.get(lensId);
-        if (!lens || !lens.uuid) {
-            lens = await DB.getSingleLens(lensId);
-            if (lens && lens[0]) {
-                lens = lens[0];
-            }
-        }
-
-        if (lens && lens.uuid) {
-            const unlockLensCombined = await Web.getUnlockByHash(lens.uuid);
-            if (unlockLensCombined) {
-                DB.insertLens(unlockLensCombined);
-                DB.insertUnlock(unlockLensCombined);
-                return res.json(unlockLensCombined);
-            }
+    } else {
+        const remoteUnlock = await getRemoteUnlockByLensId(lensId);
+        if (remoteUnlock) {
+            return res.json(remoteUnlock);
         }
     }
 
     return res.json({});
 });
+
+async function getRemoteUnlockByLensId(lensId) {
+    try {
+        if (useRelay) {
+            const unlock = await Util.getUnlockFromRelay(lensId);
+            if (unlock) {
+                DB.insertUnlock(unlock);
+                return unlock;
+            }
+        }
+
+        if (useWebSource) {
+            let lens = Web.Cache.get(lensId);
+            if (!lens || !lens.uuid) {
+                lens = await DB.getSingleLens(lensId);
+                if (lens && lens[0]) {
+                    lens = lens[0];
+                }
+            }
+
+            if (lens && lens.uuid) {
+                const unlockLensCombined = await Web.getUnlockByHash(lens.uuid);
+                if (unlockLensCombined) {
+                    DB.insertLens(unlockLensCombined);
+                    DB.insertUnlock(unlockLensCombined);
+                    return unlockLensCombined;
+                }
+            }
+        }
+    } catch (e) {
+        console.error(e);
+    }
+
+    return null;
+}
 
 export default router;
