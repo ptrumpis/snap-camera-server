b5f97f8efb36cb65f45423359f66142dbe99365b
Patrick Trumpis <patrick.trumpis@gmx.de>
2025-03-23 21:45:29 +0100

    Rework storage logic and use crawler for file downloads

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,8 +1,9 @@
+import { SnapLensWebCrawler } from '@ptrumpis/snap-lens-web-crawler';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs/promises';
 import path from 'path';
 import sharp from 'sharp';
 import { Config } from './config.js';
-import * as dotenv from 'dotenv';
-import * as fs from 'fs/promises';
 
 dotenv.config();
 
@@ -10,6 +11,9 @@ const storagePath = process.env.STORAGE_PATH;
 const storageServer = process.env.STORAGE_SERVER;
 const ignoreAltMedia = Config.app.flag.ignore_alt_media;
 const ignoreImgSequence = Config.app.flag.ignore_img_sequence;
+const validStorageUrls = Config.storage.urls || [];
+
+const Crawler = new SnapLensWebCrawler();
 
 async function saveLens(lens) {
     if (!lens) {
@@ -17,31 +21,31 @@ async function saveLens(lens) {
     }
 
     if (lens.icon_url) {
-        await savePNG(lens.icon_url);
+        await saveRemoteFile(lens.icon_url);
     }
     if (lens.snapcode_url) {
-        await savePNG(lens.snapcode_url);
+        await saveRemoteFile(lens.snapcode_url);
     }
     if (lens.thumbnail_media_url) {
-        await savePreviews(lens.thumbnail_media_url);
+        await saveRemoteFile(lens.thumbnail_media_url);
     }
     if (lens.thumbnail_media_poster_url) {
-        await savePreviews(lens.thumbnail_media_poster_url);
+        await saveRemoteFile(lens.thumbnail_media_poster_url);
     }
 
     if (!ignoreAltMedia) {
         if (lens.standard_media_url) {
-            await savePreviews(lens.standard_media_url);
+            await saveRemoteFile(lens.standard_media_url);
         }
         if (lens.standard_media_poster_url) {
-            await savePreviews(lens.standard_media_poster_url);
+            await saveRemoteFile(lens.standard_media_poster_url);
         }
 
         if (!ignoreImgSequence) {
             if (lens.image_sequence && lens.image_sequence?.size) {
                 let { url_pattern, size } = lens.image_sequence;
                 for (let i = 0; i < size; i++) {
-                    await savePreviews(url_pattern.replace('%d', i));
+                    await saveRemoteFile(url_pattern.replace('%d', i));
                 }
             }
         }
@@ -50,118 +54,8 @@ async function saveLens(lens) {
     return true;
 }
 
-async function savePreviews(url) {
-    if (typeof url !== 'string' || !url.startsWith('http') || url.startsWith(storageServer)) {
-        return false;
-    }
-
-    try {
-        let previewUrl = new URL(url);
-
-        // we need the exact same path to swap the file domain with our local storage domain
-        // but we also want some security
-        const validExtensions = ['.png', '.jpg', '.jpeg', '.mp4', '.webp'];
-        const filePath = path.normalize(path.dirname(previewUrl.pathname));
-        const fileName = path.basename(previewUrl.pathname);
-        const ext = path.extname(fileName);
-
-        // actually valid files don't need to have an extension in their URL
-        // TODO: check mime-type instead
-        if (!validExtensions.includes(ext) && !fileName.endsWith('360_640')) {
-            console.error("Unsupported Preview file extension", ext, "in URL", url);
-            return false;
-        }
-
-        // TODO: whitelist all static urls
-
-        if (filePath.includes('/preview-media/thumbnail_seq')) {
-            // community-lens.storage.googleapis.com
-            await downloadFile(previewUrl.toString(), filePath, fileName);
-        } else if (filePath.startsWith('/previewimage')) {
-            // lens-storage.storage.googleapis.com
-            const file = await downloadFile(previewUrl.toString(), filePath, fileName);
-        } else if (filePath.startsWith('/previewvideo')) {
-            // lens-storage.storage.googleapis.com
-            const file = await downloadFile(previewUrl.toString(), filePath, fileName);
-        } else if (filePath.startsWith('/previewmedia')) {
-            // lens-preview-storage.storage.googleapis.com
-            const file = await downloadFile(previewUrl.toString(), filePath, fileName);
-            await convertWebpToPng(file);
-        } else if (filePath.endsWith('/preview-media/thumbnail_poster')) {
-            // community-lens.storage.googleapis.com
-            await downloadFile(previewUrl.toString(), filePath, fileName);
-        } else if (filePath.endsWith('/preview-media/thumbnail')) {
-            // community-lens.storage.googleapis.com
-            await downloadFile(previewUrl.toString(), filePath, fileName);
-        } else if (filePath.endsWith('/preview-media/final_poster')) {
-            // community-lens.storage.googleapis.com
-            await downloadFile(previewUrl.toString(), filePath, fileName);
-        } else if (filePath.endsWith('/preview-media/final')) {
-            // community-lens.storage.googleapis.com
-            await downloadFile(previewUrl.toString(), filePath, fileName);
-        } else {
-            console.error("Unsupported Preview path", filePath, "in URL", url);
-            return false;
-        }
-    } catch (e) {
-        console.error("savePreviews error:", e, url);
-        return false;
-    }
-
-    return true;
-}
-
-// saves snapcodes and icons, supports PNG and WEBP format as of v3.*
-async function savePNG(url) {
-    if (typeof url !== 'string' || !url.startsWith('http') || url.startsWith(storageServer)) {
-        return false;
-    }
-
-    try {
-        let pngUrl = new URL(url);
-
-        // preserve original filepath
-        let filePath = path.normalize(path.dirname(pngUrl.pathname));
-        let fileName = path.basename(pngUrl.pathname);
-
-        // TODO: whitelist all static urls
-
-        if (filePath.endsWith("/png")) {
-            // snapcodes.storage.googleapis.com
-            // lens-storage.storage.googleapis.com
-            await downloadFile(pngUrl.toString(), filePath, fileName);
-        } else if (filePath.endsWith("/webp")) {
-            // lens-storage.storage.googleapis.com
-            const file = await downloadFile(pngUrl.toString(), filePath, fileName);
-            await convertWebpToPng(file);
-        } else if (filePath.endsWith("/3")) {
-            // bolt-gcdn.sc-cdn.net
-            await downloadFile(pngUrl.toString(), filePath, fileName);
-        } else if (filePath.startsWith("/web/deeplink") && fileName === "snapcode" && pngUrl.searchParams.has('data') && pngUrl.searchParams.has('type')) {
-            // app.snapchat.com
-            filePath = filePath.concat("/snapcode");
-            fileName = pngUrl.searchParams.get('data').concat('.png');
-            const regUuid = /^[a-f0-9]{32}\.png$/gi;
-            if (regUuid.test(fileName)) {
-                await downloadFile(pngUrl.toString(), filePath, fileName);
-            } else {
-                console.error("Dynamic png URL parsing missmtach", url);
-                return false;
-            }
-        } else {
-            console.error("Unsupported PNG path", filePath, "in URL", url);
-            return false;
-        }
-    } catch (e) {
-        console.error("savePNG error:", e, url);
-        return false;
-    }
-
-    return true;
-}
-
 async function saveUnlock(url) {
-    if (typeof url !== 'string' || !url.startsWith('http') || url.startsWith(storageServer)) {
+    if (!validateRemoteOrigin(url)) {
         return false;
     }
 
@@ -170,46 +64,81 @@ async function saveUnlock(url) {
         let filePath = path.normalize(path.dirname(lensUrl.pathname));
         let fileName = path.basename(lensUrl.pathname);
 
-        // whitelist all paths for now
-        if (filePath.startsWith("/") && fileName) {
-            await downloadFile(lensUrl.toString(), filePath, fileName);
-        } else {
-            console.error("Unsupported Lens path", filePath, "in URL", url);
-            return false;
-        }
+        return await downloadFile(lensUrl.toString(), filePath, fileName);
     } catch (e) {
-        console.error("saveUnlock error:", e, url);
+        console.error(`[Error] saveRemoteFile: ${url} - ${e.message}`);
+    }
+
+    return false;
+}
+
+async function saveRemoteFile(url) {
+    if (!validateRemoteOrigin(url)) {
         return false;
     }
 
-    return true;
+    try {
+        const fileUrl = new URL(url);
+
+        let filePath = path.normalize(path.dirname(fileUrl.pathname));
+        let fileName = path.basename(fileUrl.pathname);
+
+        if (filePath.startsWith("/web/deeplink") && fileName === "snapcode" && fileUrl.searchParams.has('data') && fileUrl.searchParams.has('type')) {
+            const regUuid = /^[a-f0-9]{32}\.png$/gi;
+            const dataParam = fileUrl.searchParams.get('data');
+            if (!regUuid.test(dataParam)) {
+                return false;
+            }
+
+            // enforce png file type
+            fileUrl.searchParams.set('type', 'png');
+            filePath = filePath.concat("/snapcode");
+            fileName = dataParam.concat('.png');
+        }
+
+        const file = await downloadFile(fileUrl.toString(), filePath, fileName);
+        await convertWebpToPng(file);
+
+        return true;
+    } catch (e) {
+        console.error(`[Error] saveRemoteFile: ${url} - ${e.message}`);
+    }
+
+    return false;
+}
+
+function validateRemoteOrigin(url) {
+    if (typeof url !== 'string' || !url.startsWith('http') || url.startsWith(storageServer)) {
+        return false;
+    }
+
+    for (const storageUrl of validStorageUrls) {
+        if (url.startsWith(storageUrl)) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 async function downloadFile(targetUrl, subDirectory, fileName) {
-    const dir = storagePath.concat(path.normalize(subDirectory));
-    const newFile = `${dir}/${fileName}`;
-
-    if (await isFile(newFile)) {
-        return false;
-    }
-
-    const dirExists = await isDirectory(dir);
-    if (!dirExists) {
-        await fs.mkdir(dir, { recursive: true });
-    }
-
-    console.log("Downloading", targetUrl);
-
     try {
-        const response = await fetch(targetUrl);
-        const buffer = await response.arrayBuffer();
-        await fs.writeFile(newFile, Buffer.from(buffer));
+        const newFile = path.normalize(storagePath.concat('/storage/', path.normalize(subDirectory), '/', fileName));
+        if (await isFile(newFile)) {
+            return false;
+        }
+
+        console.log(`[Downloading] ${targetUrl}`);
+
+        const result = await Crawler.downloadFile(targetUrl, newFile);
+        if (result === true) {
+            return newFile;
+        }
     } catch (e) {
-        console.error("downloadFile error:", e, targetUrl);
-        return false;
+        console.error(`[Error] downloadFile: ${targetUrl} - ${e.message}`);
     }
 
-    return newFile;
+    return false;
 }
 
 async function convertWebpToPng(file) {
@@ -217,14 +146,14 @@ async function convertWebpToPng(file) {
         if (typeof file === 'string' && file.endsWith('.webp')) {
             const fileAsPng = file.substring(0, file.lastIndexOf('.')).concat('.png');
 
-            console.log("Converting WEBP to PNG", file);
+            console.log(`[Converting WEBP to PNG] ${file}`);
 
             await sharp(file).toFile(fileAsPng);
             await fs.unlink(file);
             return fileAsPng;
         }
     } catch (e) {
-        console.error("convertWebpToPng error:", e, file);
+        console.error(`[Error] convertWebpToPng: ${file} - ${e.message}`);
     }
     return false;
 }
@@ -248,4 +177,4 @@ async function fileStat(filePath) {
     return result;
 }
 
-export { saveLens, saveUnlock, isFile, isDirectory, fileStat };
+export { saveLens, saveUnlock, isFile, isDirectory };
