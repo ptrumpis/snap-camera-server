847390988d614e0619d8f2a4b4263711132f98c7
Patrick Trumpis <patrick.trumpis@gmx.de>
2025-03-24 06:01:29 +0100

    Add periodic top lens fetching and caching

--- src/init.js
+++ src/init.js
@@ -1,7 +1,17 @@
+import SnapLensWebCrawler from '@ptrumpis/snap-lens-web-crawler/crawler';
 import dbmigrate from 'db-migrate';
+import { createRequire } from 'module';
+import { Config } from './utils/config.js';
+import * as Cache from './utils/cache.js';
 import * as DB from './utils/db.js';
 import * as Util from './utils/helper.js';
-import { createRequire } from 'module';
+
+const useWebSource = Config.app.flag.enable_web_source;
+
+const topLensesRefreshSeconds = Math.max(Config.top.interval || 0, 3600);
+const cacheTopLensesInterval = (useWebSource) ? setInterval(async () => {
+    await cacheTopLenses();
+}, topLensesRefreshSeconds * 1000).unref() : null;
 
 async function bootstrap() {
     while (!await DB.isDatabaseReady()) {
@@ -15,11 +25,20 @@ async function bootstrap() {
     process.on('SIGTERM', () => { shutdown(); });
     process.on('SIGINT', () => { shutdown(); });
 
+    if (useWebSource) {
+        await cacheTopLenses();
+    }
+
     console.log(`✅ Initialization complete!`);
 }
 
 function shutdown() {
-    console.log(`Shutting down... 💤`);
+    console.log(`💤 Shutting down...`);
+
+    if (cacheTopLensesInterval) {
+        clearInterval(cacheTopLensesInterval);
+    }
+
     process.exit(0);
 }
 
@@ -29,7 +48,7 @@ async function runDatabaseMigration() {
         await migration.up();
         console.log(`✅ Database migration complete!`);
     } catch (e) {
-        console.error(e);
+        console.error(`[Error] runDatabaseMigration: ${e.message}`);
     }
 }
 
@@ -57,16 +76,48 @@ async function prefetchStaticLenses() {
     ];
 
     try {
+        console.log(`🔄 Prefetching static lenses...`);
+
         for (let i = 0; i < staticLenses.length; i++) {
             await DB.insertLens(staticLenses[i]["lenses"]);
         }
 
         console.log(`✅ Lens prefetching complete!`);
     } catch (e) {
-        console.error(e);
+        console.error(`[Error] prefetchStaticLenses: ${e.message}`);
     }
 
     staticLenses.length = 0;
+}
+
+async function cacheTopLenses() {
+    const Crawler = new SnapLensWebCrawler(Config.top.crawler);
+
+    try {
+        console.log(`🔄 Caching top lenses...`);
+
+        for (const category in Crawler.TOP_CATEGORIES) {
+            let topLenses = await Crawler.getTopLensesByCategory(category, null);
+            if (Array.isArray(topLenses) && topLenses.length) {
+                for (const lens of topLenses) {
+                    if (!lens.lens_url) {
+                        continue;
+                    }
+
+                    Cache.TopCache.set(lens.unlockable_id, lens);
+                }
+            }
+
+            topLenses.length = 0;
+            topLenses = null;
+        }
+
+        console.log(`✅ Top lenses successfully cached!`);
+    } catch (e) {
+        console.error(`[Error] cacheTopLenses: ${e.message}`);
+    } finally {
+        Crawler.destroy();
+    }
 }
 
 export { bootstrap };
