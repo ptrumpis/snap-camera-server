5d17a16436ce91b0cc43e337a197769a9ef9c396
Patrick Trumpis <patrick.trumpis@gmx.de>
2025-03-24 02:40:54 +0100

    Rework server initialization

--- src/init.js
+++ src/init.js
@@ -3,59 +3,70 @@ import * as DB from './utils/db.js';
 import * as Util from './utils/helper.js';
 import { createRequire } from 'module';
 
-const require = createRequire(import.meta.url);
-
-const top = require('./json/top.json');
-const scheduled = require('./json/scheduled.json');
-const wfh = require('./json/wfh.json');
-const color_effect = require('./json/color_effect.json');
-const funny = require('./json/funny.json');
-const gaming = require('./json/gaming.json');
-const cute = require('./json/cute.json');
-const character = require('./json/character.json');
-
-const staticLenses = [
-    top,
-    scheduled,
-    wfh,
-    color_effect,
-    funny,
-    gaming,
-    cute,
-    character
-];
-
 async function bootstrap() {
     while (!await DB.isDatabaseReady()) {
-        console.log('⏳ Waiting for the database server to respond');
+        console.log(`⏳ Waiting for the database server to respond...`);
         await Util.sleep(6000);
     }
 
     await runDatabaseMigration();
     await prefetchStaticLenses();
 
-    console.log('Initialization complete! 🎉');
+    process.on('SIGTERM', () => { shutdown(); });
+    process.on('SIGINT', () => { shutdown(); });
+
+    console.log(`✅ Initialization complete!`);
+}
+
+function shutdown() {
+    console.log(`Shutting down... 💤`);
+    process.exit(0);
 }
 
 async function runDatabaseMigration() {
     try {
         const migration = dbmigrate.getInstance(true);
         await migration.up();
-        console.log('Database migration complete ✔️');
+        console.log(`✅ Database migration complete!`);
     } catch (e) {
         console.error(e);
     }
 }
 
 async function prefetchStaticLenses() {
+    const require = createRequire(import.meta.url);
+
+    const top = require('./json/top.json');
+    const scheduled = require('./json/scheduled.json');
+    const wfh = require('./json/wfh.json');
+    const color_effect = require('./json/color_effect.json');
+    const funny = require('./json/funny.json');
+    const gaming = require('./json/gaming.json');
+    const cute = require('./json/cute.json');
+    const character = require('./json/character.json');
+
+    const staticLenses = [
+        top,
+        scheduled,
+        wfh,
+        color_effect,
+        funny,
+        gaming,
+        cute,
+        character
+    ];
+
     try {
         for (let i = 0; i < staticLenses.length; i++) {
-            DB.insertLens(staticLenses[i]["lenses"], true);
-            await Util.sleep(1000);
+            await DB.insertLens(staticLenses[i]["lenses"]);
         }
+
+        console.log(`✅ Lens prefetching complete!`);
     } catch (e) {
         console.error(e);
     }
+
+    staticLenses.length = 0;
 }
 
 export { bootstrap };
