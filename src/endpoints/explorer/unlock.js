1ad5fb2a7c88ea2e1fa7ae77dea464a3ded5629c
Patrick Trumpis <patrick.trumpis@gmx.de>
2025-03-28 20:16:55 +0100

    Fix unlocking of new 36 char lens ID's

--- src/endpoints/explorer/unlock.js
+++ src/endpoints/explorer/unlock.js
@@ -15,28 +15,35 @@ const maxFileAgeInDays = 90;
 var router = express.Router();
 
 router.get('/', async function (req, res, next) {
-    const lensId = req?.query?.uid && parseInt(req.query.uid) || false;
+    const lensId = req?.query?.uid || false;
     if (!lensId) {
         return res.json({});
     }
 
-    const unlock = await DB.getLensUnlock(lensId);
-    if (unlock && unlock[0]) {
-        if (unlock[0].lens_id && unlock[0].lens_url) {
-            // trigger re-download to catch missing files automatically
-            await Util.downloadUnlock(unlock[0].lens_id, unlock[0].lens_url);
+    if (Util.isLensId(lensId)) {
+        const unlock = await DB.getLensUnlock(lensId);
+        if (unlock && unlock[0]) {
+            if (unlock[0].lens_id && unlock[0].lens_url) {
+                // trigger re-download to catch missing files automatically
+                await Util.downloadUnlock(unlock[0].lens_id, unlock[0].lens_url);
 
-            wayback.saveOutdatedUrl(unlock[0].lens_url, maxFileAgeInDays);
+                wayback.saveOutdatedUrl(unlock[0].lens_url, maxFileAgeInDays);
 
-            return res.json(Util.modifyResponseURLs(unlock[0]));
-        } else {
-            console.warn(`[Warning] Unlock Download URL is missing: ${lensId}`);
+                return res.json(Util.modifyResponseURLs(unlock[0]));
+            } else {
+                console.warn(`[Warning] Unlock Download URL is missing: ${lensId}`);
+            }
+        }
+
+        const remoteUnlock = await getRemoteUnlockByLensId(lensId);
+        if (remoteUnlock) {
+            return res.json(remoteUnlock);
         }
     }
 
-    const remoteUnlock = await getRemoteUnlockByLensId(lensId);
-    if (remoteUnlock) {
-        return res.json(remoteUnlock);
+    const cacheUnlock = await getCacheUnlockByLensId(lensId);
+    if (cacheUnlock) {
+        return res.json(cacheUnlock);
     }
 
     console.info(`[Info] 😕 This lens cannot currently be activated: ${lensId}`);
@@ -53,36 +60,59 @@ async function getRemoteUnlockByLensId(lensId) {
                 return unlock;
             }
         }
+    } catch (e) {
+        console.error(e);
+    }
 
+    return null;
+}
+
+async function getCacheUnlockByLensId(lensId) {
+    try {
         if (useWebSource) {
             if (Cache.Top.has(lensId)) {
                 const topLens = Cache.Top.get(lensId);
-                DB.insertLens(topLens);
-                DB.insertUnlock(topLens);
-                return topLens;
+                return unlockLens(topLens);
             }
 
             if (Cache.Search.has(lensId)) {
                 let lens = Cache.Search.get(lensId);
                 if (!lens?.uuid) {
                     const dbLens = await DB.getSingleLens(lensId);
-                    lens = dbLens?.[0] ? { ...lens, ...dbLens } : lens;
+                    lens = (dbLens?.[0]) ? Util.mergeLens(dbLens, lens) : lens;
+                }
+
+                if (lens?.uuid && !Util.isLensId(lensId)) {
+                    const webLens = await Web.getLensByHash(lens.uuid);
+                    lens = (webLens) ? Util.mergeLens(webLens, lens) : lens;
                 }
 
                 if (lens?.uuid && !lens?.lens_url) {
                     const webLens = await Web.getUnlockByHash(lens.uuid);
-                    lens = webLens ? { ...webLens, ...lens } : lens;
+                    lens = (webLens) ? Util.mergeLens(webLens, lens) : lens;
                 }
 
-                if (lens?.lens_url) {
-                    DB.insertLens(lens);
-                    DB.insertUnlock(lens);
-                    return lens;
+                if (lens?.lens_id && !Util.isLensId(lens.lens_id) && Util.isLensId(lens.unlockable_id)) {
+                    lens.lens_id = lens.unlockable_id;
                 }
+
+                Cache.Search.set(lensId, lens);
+
+                return unlockLens(lens);
             }
         }
     } catch (e) {
         console.error(e);
+    }
+
+    return null;
+}
+
+function unlockLens(lens) {
+    if (lens?.lens_url && Util.isLensId(lens.unlockable_id) && Util.isLensId(lens.lens_id)) {
+        DB.insertLens(lens);
+        DB.insertUnlock(lens);
+        return lens;
     }
 
     return null;
