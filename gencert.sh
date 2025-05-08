c42f5ed6443e827d2e0e936f5909d20f980a3c77
Patrick Trumpis <patrick.trumpis@gmx.de>
2025-05-09 00:55:51 +0200

    Update file check condition

--- gencert.sh
+++ gencert.sh
@@ -3,7 +3,7 @@
 KEY_FILE=./ssl/studio-app.snapchat.com.key
 CRT_FILE=./ssl/studio-app.snapchat.com.crt
 
-if [[ ! -f "$KEY_FILE" || ! -f "$CRT_FILE" ]]; then
+if [ ! -f "$KEY_FILE" ] || [ ! -f "$CRT_FILE" ]; then
    openssl req -x509 -nodes -days 3650 \
       -subj "/C=CA/ST=QC/O=Snap Inc./CN=studio-app.snapchat.com" \
       -addext "subjectAltName=DNS:*.snapchat.com" \
