5fe71253df38de8425edb9022e19b7c76cd8060b
Patrick Trumpis <patrick.trumpis@gmx.de>
2025-05-08 19:24:02 +0200

    Prevent gencert to overwrite existing certificate files

--- gencert.sh
+++ gencert.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-openssl req -x509 -nodes -days 3650 -subj "/C=CA/ST=QC/O=Snap Inc./CN=studio-app.snapchat.com" -addext "subjectAltName=DNS:*.snapchat.com" -newkey rsa:2048 -keyout ./ssl/studio-app.snapchat.com.key -out ./ssl/studio-app.snapchat.com.crt
+
+KEY_FILE=./ssl/studio-app.snapchat.com.key
+CRT_FILE=./ssl/studio-app.snapchat.com.crt
+
+if [[ ! -f "$KEY_FILE" || ! -f "$CRT_FILE" ]]; then
+   openssl req -x509 -nodes -days 3650 \
+      -subj "/C=CA/ST=QC/O=Snap Inc./CN=studio-app.snapchat.com" \
+      -addext "subjectAltName=DNS:*.snapchat.com" \
+      -newkey rsa:2048 -keyout "$KEY_FILE" -out "$CRT_FILE"
+else
+   echo "SSL certificate already exists."
+fi
